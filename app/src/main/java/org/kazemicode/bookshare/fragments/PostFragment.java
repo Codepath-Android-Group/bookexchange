881166eacd3fe63f26a7c4b8cfe829a3f277bd36
kazemicode <sarieses@gmail.com>
2020-12-01 16:35:18 -0800

    Added post feature

--- app/src/main/java/org/kazemicode/bookshare/fragments/PostFragment.java
+++ app/src/main/java/org/kazemicode/bookshare/fragments/PostFragment.java
@@ -2,13 +2,24 @@ package org.kazemicode.bookshare.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import org.kazemicode.bookshare.R;
+import org.kazemicode.bookshare.models.Post;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,51 +27,120 @@ import org.kazemicode.bookshare.R;
  * create an instance of this fragment.
  */
 public class PostFragment extends Fragment {
+    private EditText etISBN;
+    private EditText etTitle;
+    private EditText etAuthor;
+    private EditText etImgURL;
+    private EditText etType;
+    private EditText etCustom;
+    private Button btnSubmit;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static final String TAG = "PostFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public PostFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PostFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
+    @SuppressWarnings("unused")
     public static PostFragment newInstance(String param1, String param2) {
         PostFragment fragment = new PostFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_post, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        etISBN = view.findViewById(R.id.etISBN);
+        etAuthor = view.findViewById(R.id.etAuthor);
+        etTitle = view.findViewById(R.id.etTitle);
+        etImgURL = view.findViewById(R.id.etImgURL);
+        etType = view.findViewById(R.id.etType);
+        etCustom = view.findViewById(R.id.etCustom);
+        btnSubmit = view.findViewById(R.id.btnPost);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String isbn = etISBN.getText().toString();
+                String author = etAuthor.getText().toString();
+                String title = etTitle.getText().toString();
+                String imgUrl = etImgURL.getText().toString();
+                String type = etType.getText().toString();
+                String custom = etCustom.getText().toString();
+
+                if(isbn.isEmpty()){
+                    Toast.makeText(getContext(), "isbn is required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(author.isEmpty()){
+                    Toast.makeText(getContext(), "author is required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(title.isEmpty()){
+                    Toast.makeText(getContext(), "title is required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(type.isEmpty()){
+                    Toast.makeText(getContext(), "type is required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(custom.isEmpty()){
+                    Toast.makeText(getContext(), "description is required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                savePost(isbn, author, title, imgUrl,type, custom, currentUser);
+
+
+            }
+        });
+
+    }
+
+    private void savePost(String isbn, String author, String title, String imgUrl, String type, String custom, ParseUser user) {
+        Post post = new Post();
+        post.setCustomDescription(custom);
+        post.setTitle(title);
+        post.setISBN(isbn);
+        post.setAuthor(author);
+        post.setImgUrl(imgUrl);
+        post.setPoster(user);
+        post.setType(type);
+        https://s1.nyt.com/du/books/images/9780765326386.jpg
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null){
+                    // there's a problem
+                    Log.e(TAG, "Problem saving post", e);
+                    Toast.makeText(getContext(), "Problem saving post", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+
+                    Log.i(TAG, "Post saved");
+                    etCustom.setText("");
+                    etTitle.setText("");
+                    etAuthor.setText("");
+                    etImgURL.setText("");
+                    etType.setText("");
+                    etISBN.setText("");
+
+
+                }
+            }
+        });
     }
 }
